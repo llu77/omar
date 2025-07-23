c8672c081a6a703ed6b83efda7190c7b9b9e525a
عمر المطيري <nntn127@gmail.com>
2025-07-23 10:04:41 +0000

    لا الربط فط مع اوبن اي ايى

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,10 +1,10 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {openAI} from 'genkitx-openai';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      apiKey: process.env.GEMINI_API_KEY,
+    openAI({
+      apiKey: process.env.OPENAI_API_KEY,
     }),
   ],
 });
