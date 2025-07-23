4090f39c33e1a6c48d86b4d82627d07a422750fa
عمر المطيري <nntn127@gmail.com>
2025-07-23 09:26:34 +0000

    الغي متطلب مفتاح جيمناي واكتفي بمفتاح اوبن ايايا\

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -4,7 +4,7 @@ import {openAI} from 'genkitx-openai';
 export const ai = genkit({
   plugins: [
     openAI({
-      apiKey: process.env.OPENAI_API_KEY || "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",
+      apiKey: process.env.OPENAI_API_KEY,
     }),
   ],
 });
