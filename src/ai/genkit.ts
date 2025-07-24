4a8fc8fd2f01b80e0b13a48c6db430d5f51479bc
عمر المطيري <nntn127@gmail.com>
2025-07-24 16:00:45 +0000

    react-dom.development.js:38560 Download the React DevTools for a better

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,18 +3,15 @@
  */
 
 import {genkit} from '@genkit-ai/ai';
-import {configureGenkit} from '@genkit-ai/core';
 import {googleAI} from '@genkit-ai/googleai';
 import {z} from 'zod';
 
 export {z};
 
-configureGenkit({
+export const ai = genkit({
   plugins: [
     googleAI(),
   ],
   logLevel: 'debug',
   enableTracingAndMetrics: true,
 });
-
-export const ai = genkit();
